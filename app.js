d1c8a3564acdee94b34af73a90c15994827faa92
Toby.Broadbent <tobybroadbent@aol.com>
2021-11-17 10:40:27 +0000

    My first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let myFunction = () => {
+
+    document.getElementById('change').innerHTML = 'Form Submitted';
+    
+    
+    
+    }
+
+    function changingColour() {
+
+        var element = document.body;
+        element.classList.toggle("Color-change")
+        var Nav = document.getElementById("navBar")
+        var Footer = document.getElementById("footer")
+        Nav.classList.toggle("ColorChangeNav")
+        Footer.classList.toggle("ColorChangeNav")
+
+
+
+
+    }
+
+    const para = document.createElement("p");
+    const node = document.createTextNode("Hope this works");
+    para.appendChild(node);
+    const element = document.getElementById("ggg");
+    element.appendChild(para);
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "CLICK ME";
+    element.appendChild(btn);
